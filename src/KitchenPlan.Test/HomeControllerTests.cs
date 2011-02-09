6b8159a89c447adfaeade692d6c077539bf29a7c
Katherine Rayburn <kate@dragonlanding.com>
2011-02-08 21:25:29 -0600

    Initial Views and Tests

---
+++ src/KitchenPlan.Test/HomeControllerTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using KitchenPlan.Controllers;
+using System.Web.Mvc;
+
+namespace KitchenPlan.Test
+{
+
+    [TestClass]
+    public class HomeControllerTests
+    {
+
+        [TestMethod]
+        public void Index_Should_Return_A_View_With_A_Title()
+        {
+            //Arrange
+            var cntrl = new HomeController();
+
+            //Act
+            var result = cntrl.Index();
+
+            //Assert
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+            var view = result as ViewResult;
+            Assert.AreEqual("Home", view.ViewBag.Title);
+        }
+        [TestMethod]
+        public void About_Should_Return_A_View_With_A_Title()
+        {
+            //Arrange
+            var cntrl = new HomeController();
+
+            //Act
+            var result = cntrl.About();
+
+            //Assert
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+            var view = result as ViewResult;
+            Assert.AreEqual("Home", view.ViewBag.Title);
+        }
+    }
+}
---
+++ src/KitchenPlan/Controllers/MeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KitchenPlan.Controllers
+{
+    public class MeController : Controller
+    {
+        //
+        // GET: /Me/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
